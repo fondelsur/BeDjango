e7ec71c6c14a2ba4ec3eaa5d8494fa1dc3049eff
Alfonso Rodriguez <fondelsur@gmail.com>
2016-07-12 10:28:22 +0200

    update

--- paste/notes/models.py
+++ paste/notes/models.py
@@ -9,6 +9,13 @@ class Notes(models.Model):
 										auto_now_add=True)
 	addresee = models.CharField(verbose_name='Addresee', blank=True, null=True,
 								max_length=255)
+	title = models.CharField(verbose_name='Title', max_length=20, default="White title")
+
+	def __str__(self):
+		return self.title
+
+	def __unicode__(self):
+		return self.title
 
 	class Meta(object):
 		verbose_name = 'Note'
